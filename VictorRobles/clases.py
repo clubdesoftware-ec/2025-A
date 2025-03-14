392da7dbd08ba94cd8720db2029be04d5caccfc6
Victor <roblesvictor02@gmail.com>
2025-03-13 21:32:31 -0500

    clases

--- VictorRobles/clases.py
+++ VictorRobles/clases.py
@@ -16,9 +16,16 @@ class Perro:
         self.x += 1
     
     def MoverEnY(self):
-        self.y = += 1
+        self.y += 1
 
 dog = Perro()
+dog2 = Perro()
 print(dog.nombre)
 dog.moverEnX()
 print(dog.x)
+print(dog2.x)
+
+
+l = [32, 234, 123]
+l.append(54)
+print(l)
