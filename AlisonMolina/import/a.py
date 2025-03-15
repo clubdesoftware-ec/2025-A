d25cf0867e5a0440a3eb69d0b26b9c29c235c828
Alison Molina <molinalizeth18@gmail.com>
2025-03-14 20:25:02 -0500

    import

--- AlisonMolina/import/a.py
+++ AlisonMolina/import/a.py
@@ -1,4 +1,16 @@
-print("from a")
-import b
+#print("from a")
+#import b
 
-b.impl1()
+#b.impl1()
+
+#import b
+from b import impl1 as fn
+
+import b as otro
+
+def impl1():
+    print("hello from b")
+    
+fn()
+otro.impl1()
+## uk = b.Ukelele()
