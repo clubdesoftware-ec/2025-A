de6195d4dee786341052ee80a85ca0b37b50bf1e
Victor <roblesvictor02@gmail.com>
2025-03-25 22:21:32 -0500

    cambios 25/03

--- proyectos/LogiCost/Calculadora.py
+++ proyectos/LogiCost/Calculadora.py
@@ -15,21 +15,20 @@ if calculadora == "Costo de transporte":
     km_por_viaje = st.number_input("Kilómetros recorridos por viaje", min_value=0.0, step=0.1)
     tarifa_por_tonelada = st.number_input("Tarifa actual por tonelada", min_value=0.0, step=0.1)
 
-    # Sección: Costos Fijos
-    st.header("Costos Fijos")
-    salario_conductor = st.number_input("Salario del conductor", min_value=0.0, step=0.1)
-    costo_seguros = st.number_input("Costo de seguros", min_value=0.0, step=0.1)
-    depreciacion_vehiculo = st.number_input("Depreciación del vehículo", min_value=0.0, step=0.1)
-    costo_matricula = st.number_input("Costo de matrícula", min_value=0.0, step=0.1)
-    costo_inversion = st.number_input("Costo de inversión", min_value=0.0, step=0.1)
-
     # Sección: Costos Variables
     st.header("Costos Variables")
-    consumo_combustible = st.number_input("Consumo de combustible (litros/km o galones/km)", min_value=0.0, step=0.01)
-    precio_combustible = st.number_input("Precio del combustible por litro o galón", min_value=0.0, step=0.01)
-    costo_llantas = st.number_input("Costo de llantas", min_value=0.0, step=0.1)
-    vida_util_llantas = st.number_input("Vida útil de las llantas (en kilómetros)", min_value=0.0, step=0.1)
-    costo_mantenimiento = st.number_input("Costos de mantenimiento preventivo y correctivo", min_value=0.0, step=0.1)
+    gasto_combustible_anual = st.number_input("Gasto de combustible anual", min_value=0.0, step=0.1)
+    gasto_neumaticos_anual = st.number_input("Gasto en neumáticos anual", min_value=0.0, step=0.1)
+    gasto_mantenimiento_preventivo_anual = st.number_input("Gasto en mantenimiento preventivo anual", min_value=0.0, step=0.1)
+    gasto_mantenimiento_correctivo_anual = st.number_input("Gasto en mantenimiento correctivo anual", min_value=0.0, step=0.1)
+
+    # Sección: Costos Fijos
+    st.header("Costos Fijos")
+    gastos_anuales_mano_obra = st.number_input("Gastos anuales en mano de obra", min_value=0.0, step=0.01)
+    gastos_legalizacion_año = st.number_input("Gastos en legalización al año", min_value=0.0, step=0.01)
+    depreciacion_anual = st.number_input("Depreciación anual", min_value=0.0, step=0.1)
+    gastos_administrativos_anuales = st.number_input("Gastos administrativos anuales", min_value=0.0, step=0.1)
+    costo_inversion = st.number_input("Costos de inversión", min_value=0.0, step=0.1)
 
 elif calculadora == "Rendimiento de combustible":
 
@@ -61,7 +60,34 @@ elif calculadora == "Rendimiento de combustible":
                 st.write(f"{precio_promedio_galon}/galón")
     with col2:
         rendimiento_galon = st.number_input("Rendimiento del galón (Km/galón)",
-                                                min_value=0.1, value=5.0, step=0.1) 
+                                                min_value=0.1, value=5.0, step=0.1)
+
+    # Sección: Costo del combustible mensual        
+    st.header("Costo del combustible mensual")
+    col1, col2 = st.columns([2, 1])
+
+    with col1:
+        costo_kilometro_recorrido = st.number_input("Costo por kilómetro recorrido ($)",
+                                                    min_value=0.1, value=0.1, step=0.1)
+        
+    with col2:
+        kilometro_recorrido_mes = st.number_input("Kilómetros recorridos al mes",
+                                                    min_value=0.1, value=1000.0, step=0.1)
+        
+    # Sección: Costo del combustible anual
+    st.header("Costo del combustible anual")
+    col1, col2 = st.columns([2, 1])
+        
+    with col1:
+        kilometro_recorrido_anual = st.number_input("Kilómetros recorridos al año",
+                                                    min_value=0.1, value=12000.0, step=0.1)
+        
+        
+        
+    
+
+
+
 
 # Botón para calcular
 if st.button("Calcular"):   
